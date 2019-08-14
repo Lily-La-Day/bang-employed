44e697852946ccde7307012c49d8e11a2c968aa6
Lily Keeling <lily.keeling@gmail.com>
2019-08-14 11:54:58 +0100

    deploying

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000
-const dbURI =  'mongodb://localhost/projects'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/projects'
 const secret = process.env.SECRET || 'secret sauce'
 
 module.exports = {
